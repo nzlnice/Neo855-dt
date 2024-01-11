7455b217650de4ee5398ceff8cf97901395ae5ab
nzlnice <3193782263@qq.com>
2024-01-11 17:12:39 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Neo855-dt
-twrpdtgen自动写的dt测试
+# Android device tree for vivo V1936A (PD1936)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_PD1936-user
+add_lunch_combo omni_PD1936-userdebug
+add_lunch_combo omni_PD1936-eng
